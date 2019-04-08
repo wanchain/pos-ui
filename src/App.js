e0c46411bebdaf88083cc0904fed08894351f4b8
MoLin <molin@wanchain.org>
2019-04-08 20:59:44 +0800

    update.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      blockNumber: 0,
+      totalStake: 0,
+      minerCount: 0,
+      delegatorCount: 0,
+      delePartiCnt: 0,
+    }
+
+    
+  }
+
+  componentDidMount() {
+    const socket = io.connect('http://localhost:8000/my-namespace');
+
+    socket.on('info', (info) => {
+      this.setState({
+        blockNumber: info.blockNumber,
+        totalStake: info.totalStake,
+        minerCount: info.minerCount,
+        delegatorCount: info.delegatorCount,
+        delePartiCnt: info.delePartiCnt,
+      })
+    });
+
+    this.timer = setInterval(()=>{
+      socket.emit('getInfo', '')
+    }, 5000, null)
+    this.socket = socket
+  }
+
+  componentWillUnmount() {
+    this.socket.close()
+    clearInterval(this.timer)
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div className="title">Wanchain PoS Status</div>
+          <div>
+            <div>Block Number: {this.state.blockNumber}</div>
+            <div>Total Stake: {this.state.totalStake} wanCoins</div>
+            <div>Miner count: {this.state.minerCount}</div>
+            <div>Delegator count: {this.state.delegatorCount}</div>
+            <div>Delegate participant count: {this.state.delePartiCnt}</div>
+          </div>
+
         </header>
       </div>
     );
