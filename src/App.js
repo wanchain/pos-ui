861e5c154eeac3542df9f42500005550201d9855
MoLin <molin@wanchain.org>
2019-04-09 15:37:49 +0800

    update to use http.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import io from 'socket.io-client';
+import $ from 'jquery';
 
+const serverUrl = 'http://localhost:8000/'
 
 class App extends Component {
   constructor(props) {
@@ -13,103 +13,133 @@ class App extends Component {
       minerCount: 0,
       delegatorCount: 0,
       delePartiCnt: 0,
-      minerTotalReward: 0,
-      minerRewardRate: 0,
+      minerTotalReward: 'N/A',
+      minerRewardRate: 'N/A',
+      delegateTotalReward: 'N/A',
+      delegateRewardRate: 'N/A',
     }
+  }
 
-
+  getInfo() {
+    this.serverRequest = $.get(serverUrl + 'info', function (result) {
+      console.log(result)
+      this.setState({
+        blockNumber: result.blockNumber,
+        totalStake: result.totalStake,
+        minerCount: result.minerCount,
+        delegatorCount: result.delegatorCount,
+        delePartiCnt: result.delePartiCnt,
+      });
+    }.bind(this));
   }
 
   componentDidMount() {
-    const socket = io.connect('http://localhost:8000/my-namespace');
-
-    socket.on('info', (info) => {
-      this.setState({
-        blockNumber: info.blockNumber,
-        totalStake: info.totalStake,
-        minerCount: info.minerCount,
-        delegatorCount: info.delegatorCount,
-        delePartiCnt: info.delePartiCnt,
-      })
-    });
-
-    socket.on('calcMiner', (info) => {
-      console.log('result comed')
-      this.setState({
-        minerTotalReward: info.minerTotalReward,
-        minerRewardRate: info.minerRewardRate,
-      })
-    })
-
-    this.timer = setInterval(() => {
-      socket.emit('getInfo', '')
-    }, 5000, null)
-    this.socket = socket
+    this.getInfo()
+    this.timer = setInterval(this.getInfo.bind(this), 5000, null)
   }
 
   componentWillUnmount() {
-    this.socket.close()
     clearInterval(this.timer)
+    this.serverRequest.abort();
   }
 
   minerCalc() {
     console.log('minerCalc')
     this.setState({
-      minerTotalReward: 0,
-      minerRewardRate: 0,
+      minerTotalReward: 'Waiting',
+      minerRewardRate: 'Waiting',
     })
-    this.socket.emit('calcMiner', {
-      amount: this.minerLockAmount.value,
-      locktime: this.minerLockTime.value
+
+    let amount = this.minerLockAmount.value
+    let locktime = this.minerLockTime.value
+
+    this.serverRequest = $.get(serverUrl + 'minerCalc?amount=' + amount + '&locktime=' + locktime,
+      function (result) {
+        console.log(result)
+        this.setState({
+          minerTotalReward: result.minerTotalReward.toFixed(2),
+          minerRewardRate: result.minerRewardRate.toFixed(2) + '%',
+        });
+      }.bind(this));
+  }
+
+  delegateCalc() {
+    console.log('delegateCalc')
+    this.setState({
+      delegateTotalReward: 'Waiting',
+      delegateRewardRate: 'Waiting',
     })
+
+    let amount = this.delegateAmount.value
+    let locktime = this.delegateLockTime.value
+    let feerate = this.delegateFeeRate.value
+
+    this.serverRequest = $.get(serverUrl + 'delegateCalc?amount=' +
+      amount + '&locktime=' + locktime + '&feerate=' + feerate,
+      function (result) {
+        console.log(result)
+        this.setState({
+          delegateTotalReward: result.delegateTotalReward.toFixed(2),
+          delegateRewardRate: result.delegateRewardRate.toFixed(2) + '%',
+        });
+      }.bind(this));
   }
 
   render() {
     return (
-      <div class="grid-container">
-        <div class="title">Wanchain POS Incentive Calculator</div>
-        <div class="blkN0Title">Block Number:</div>
-        <div class="blkNoValue">{this.state.blockNumber}</div>
-        <div class="totalstkTitle">Total Stake:</div>
-        <div class="totalstkValue">{this.state.totalStake}</div>
-        <div class="everageTitle">Average Reward:</div>
-        <div class="everagetValue">{2100000 * 100 / (this.state.totalStake)}%</div>
-        <div class="minerCntTitle">Miner Count:</div>
-        <div class="minerCntValue">{this.state.minerCount}</div>
-        <div class="delegatorCntTitle">Delegator Count:</div>
-        <div class="delegatorCntValue">{this.state.delegatorCount}</div>
-        <div class="delegatePartCntTitle">Delegator Participant:</div>
-        <div class="delegatePartCntValue">{this.state.delePartiCnt}</div>
-        <div class="calculateMinerTitle">Miner reward calculator:</div>
-        <div class="calcMiner">
-          <div class="lockAmount">
+      <div className="grid-container">
+        <div className="title">Wanchain POS Incentive Calculator</div>
+        <div className="blkN0Title">Block Number:</div>
+        <div className="blkNoValue">{this.state.blockNumber}</div>
+        <div className="totalstkTitle">Total Stake:</div>
+        <div className="totalstkValue">{this.state.totalStake}</div>
+        <div className="everageTitle">Average Reward:</div>
+        <div className="everagetValue">{2100000 * 100 / (this.state.totalStake)}%</div>
+        <div className="minerCntTitle">Miner Count:</div>
+        <div className="minerCntValue">{this.state.minerCount}</div>
+        <div className="delegatorCntTitle">Delegator Count:</div>
+        <div className="delegatorCntValue">{this.state.delegatorCount}</div>
+        <div className="delegatePartCntTitle">Delegator Participant:</div>
+        <div className="delegatePartCntValue">{this.state.delePartiCnt}</div>
+        <div className="calculateMinerTitle">Miner reward calculator:</div>
+        <div className="calcMiner">
+          <div className="lockAmount">
             <input placeholder="Lock Amount"
               ref={(input) => { this.minerLockAmount = input }}
             /></div>
-          <div class="lockTime">
+          <div className="lockTime">
             <input placeholder="Lock Time"
               ref={(input) => { this.minerLockTime = input }}
             /></div>
-          <div class="calcButton"><button onClick={this.minerCalc.bind(this)}>Calculate</button></div>
-          <div class="totalReward">Total Reward:</div>
-          <div class="totalRewardValue">
-          {this.state.minerTotalReward.toFixed(2)}
+          <div className="calcButton"><button onClick={this.minerCalc.bind(this)}>Calculate</button></div>
+          <div className="totalReward">Total Reward:</div>
+          <div className="totalRewardValue">
+            {this.state.minerTotalReward}
           </div>
-          <div class="rewardRate">Reward Rate:</div>
-          <div class="rewardRateValue">{this.state.minerRewardRate.toFixed(2)}%</div>
+          <div className="rewardRate">Reward Rate:</div>
+          <div className="rewardRateValue">{this.state.minerRewardRate}</div>
         </div>
-        <div class="calculateDelegatorTitle">Send delegate reward calculator:</div>
-        <div class="calcDelegate">
-          <div class="inputDelegate"><input placeholder="Lock Amount" /></div>
-          <div class="inputDelegateTime"><input placeholder="Lock Time" /></div>
-          <div class="inputDelegateFeerate"><input placeholder="Delegator Fee Rate" /></div>
-          <div class="deleButton"><button>Calculate</button></div>
-          <div class="totalDelegateReward">Total Reward:</div>
-          <div class="totalDelegateRewardValue">{}</div>
-          <div class="deleRewardRate">Reward Rate:</div>
-          <div class="deleRewardRateValue">{}</div>
+        <div className="calculateDelegatorTitle">Send delegate reward calculator:</div>
+        <div className="calcDelegate">
+          <div className="inputDelegate">
+            <input placeholder="Lock Amount"
+              ref={(input) => { this.delegateAmount = input }}
+            /></div>
+          <div className="inputDelegateTime">
+            <input placeholder="Lock Time"
+              ref={(input) => { this.delegateLockTime = input }}
+            /></div>
+          <div className="inputDelegateFeerate">
+            <input placeholder="Delegator Fee Rate"
+              ref={(input) => { this.delegateFeeRate = input }}
+            /></div>
+          <div className="deleButton">
+            <button onClick={this.delegateCalc.bind(this)}>Calculate</button></div>
+          <div className="totalDelegateReward">Total Reward:</div>
+          <div className="totalDelegateRewardValue">{this.state.delegateTotalReward}</div>
+          <div className="deleRewardRate">Reward Rate:</div>
+          <div className="deleRewardRateValue">{this.state.delegateRewardRate}</div>
         </div>
-
       </div>
     );
   }
