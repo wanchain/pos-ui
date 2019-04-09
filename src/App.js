c69308997935972738cb9993bc226d2b5b19d0d9
MoLin <molin@wanchain.org>
2019-04-09 14:43:05 +0800

    update with ws.

--- src/App.js
+++ src/App.js
@@ -13,9 +13,11 @@ class App extends Component {
       minerCount: 0,
       delegatorCount: 0,
       delePartiCnt: 0,
+      minerTotalReward: 0,
+      minerRewardRate: 0,
     }
 
-    
+
   }
 
   componentDidMount() {
@@ -31,7 +33,15 @@ class App extends Component {
       })
     });
 
-    this.timer = setInterval(()=>{
+    socket.on('calcMiner', (info) => {
+      console.log('result comed')
+      this.setState({
+        minerTotalReward: info.minerTotalReward,
+        minerRewardRate: info.minerRewardRate,
+      })
+    })
+
+    this.timer = setInterval(() => {
       socket.emit('getInfo', '')
     }, 5000, null)
     this.socket = socket
@@ -42,21 +52,64 @@ class App extends Component {
     clearInterval(this.timer)
   }
 
+  minerCalc() {
+    console.log('minerCalc')
+    this.setState({
+      minerTotalReward: 0,
+      minerRewardRate: 0,
+    })
+    this.socket.emit('calcMiner', {
+      amount: this.minerLockAmount.value,
+      locktime: this.minerLockTime.value
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <div className="title">Wanchain PoS Status</div>
-          <div>
-            <div>Block Number: {this.state.blockNumber}</div>
-            <div>Total Stake: {this.state.totalStake} wanCoins</div>
-            <div>Miner count: {this.state.minerCount}</div>
-            <div>Delegator count: {this.state.delegatorCount}</div>
-            <div>Delegate participant count: {this.state.delePartiCnt}</div>
+      <div class="grid-container">
+        <div class="title">Wanchain POS Incentive Calculator</div>
+        <div class="blkN0Title">Block Number:</div>
+        <div class="blkNoValue">{this.state.blockNumber}</div>
+        <div class="totalstkTitle">Total Stake:</div>
+        <div class="totalstkValue">{this.state.totalStake}</div>
+        <div class="everageTitle">Average Reward:</div>
+        <div class="everagetValue">{2100000 * 100 / (this.state.totalStake)}%</div>
+        <div class="minerCntTitle">Miner Count:</div>
+        <div class="minerCntValue">{this.state.minerCount}</div>
+        <div class="delegatorCntTitle">Delegator Count:</div>
+        <div class="delegatorCntValue">{this.state.delegatorCount}</div>
+        <div class="delegatePartCntTitle">Delegator Participant:</div>
+        <div class="delegatePartCntValue">{this.state.delePartiCnt}</div>
+        <div class="calculateMinerTitle">Miner reward calculator:</div>
+        <div class="calcMiner">
+          <div class="lockAmount">
+            <input placeholder="Lock Amount"
+              ref={(input) => { this.minerLockAmount = input }}
+            /></div>
+          <div class="lockTime">
+            <input placeholder="Lock Time"
+              ref={(input) => { this.minerLockTime = input }}
+            /></div>
+          <div class="calcButton"><button onClick={this.minerCalc.bind(this)}>Calculate</button></div>
+          <div class="totalReward">Total Reward:</div>
+          <div class="totalRewardValue">
+          {this.state.minerTotalReward.toFixed(2)}
           </div>
+          <div class="rewardRate">Reward Rate:</div>
+          <div class="rewardRateValue">{this.state.minerRewardRate.toFixed(2)}%</div>
+        </div>
+        <div class="calculateDelegatorTitle">Send delegate reward calculator:</div>
+        <div class="calcDelegate">
+          <div class="inputDelegate"><input placeholder="Lock Amount" /></div>
+          <div class="inputDelegateTime"><input placeholder="Lock Time" /></div>
+          <div class="inputDelegateFeerate"><input placeholder="Delegator Fee Rate" /></div>
+          <div class="deleButton"><button>Calculate</button></div>
+          <div class="totalDelegateReward">Total Reward:</div>
+          <div class="totalDelegateRewardValue">{}</div>
+          <div class="deleRewardRate">Reward Rate:</div>
+          <div class="deleRewardRateValue">{}</div>
+        </div>
 
-        </header>
       </div>
     );
   }
