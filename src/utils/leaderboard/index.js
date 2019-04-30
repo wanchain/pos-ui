e48b1bf775e76e4f01170bbabb12ffe1f00cdfce
Mo Lin <molin@wanchain.org>
2019-04-30 16:01:46 +0800

    update ui.

--- src/utils/leaderboard/index.js
+++ src/utils/leaderboard/index.js
@@ -39,16 +39,24 @@ class LeaderBoard extends Component {
   render() {
     const snap = this.state.leaderBoard.slice(0, this.state.leaderBoard.length)
     const items = snap.map((value, index) => {
+      let delegateAmount = 0
+      for (let i = 0; i < value.Clients.length; i++) {
+        delegateAmount += value.Clients[i].Amount;
+      }
+      delegateAmount /= 1e18;
+      let delePercent = delegateAmount * 100 / (value.Amount * 5)
+      
       return (
         <li className="liGrid" key={index}>
-          <div className="liAddr">Address:</div>
+          <div className="liAddr">Addr:</div>
           <div className="liAddrValue">{value.Address}</div>
           <div className="liAmount">Amount:</div>
           <div className="liAmountValue">{value.Amount}</div>
-          <div className="liStartEpoch">Work Epoch:</div>
+          <div className="liStartEpoch">Epoch:</div>
           <div className="liStartEpochValue">{value.StakingEpoch} ~ {(value.StakingEpoch + value.LockEpochs)}</div>
           <div className="liEndEpoch">FeeRate: {value.FeeRate}</div>
           <div className="liEndEpochValue">Delegator: {value.Clients.length}</div>
+          <div className="liDelegatorAmount">Delegate Percent: {delePercent.toFixed(0)}%</div>
         </li>
       );
     })
