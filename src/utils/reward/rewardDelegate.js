c4182f861428567ddb940edd74afc5207586a116
Mo Lin <molin@wanchain.org>
2019-05-15 15:38:51 +0800

    update ui.

--- src/utils/reward/rewardDelegate.js
+++ src/utils/reward/rewardDelegate.js
@@ -49,13 +49,13 @@ class RewardDelegate extends Component {
         <div className="rewardGrid">
           <div className="i">
             <div className="i1">Amount:</div>
-            <div className="i2" hidden="true">Lock Time:</div>
+            <div className="i3">Lock Time:</div>
             <div className="i2">Fee Rate:</div>
             <div className="bt"><button onClick={this.delegateCalc.bind(this)}>Calculate</button></div>
             <div className="i11"><input placeholder="Lock Amount In Wan Coins"
               ref={(input) => { this.delegateAmount = input }}
             ></input></div>
-            <div className="i21" hidden="true"><input placeholder="Lock Time In Epochs"
+            <div className="i31"><input placeholder="Lock Time In Epochs"
             value="7" readOnly={true}></input></div>
             <div className="i21"><input placeholder="Delegator's Fee Rate 10 (%)"
               ref={(input) => { this.delegateFeeRate = input }}
