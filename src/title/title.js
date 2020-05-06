19b550f84dfbfc0920d5d71f459aa70ed0f650be
Mo Lin <molin@wanchain.org>
2020-05-06 18:18:10 +0800

    update ui.

--- src/title/title.js
+++ src/title/title.js
@@ -7,8 +7,8 @@ class Title extends Component {
     return (
       <div className="title">
         <div>Wanchain PoS Calculator</div>
-        <Tag color="#2db7f5" className="tags">Testnet</Tag>
-        {/* <Tag color="#2db7f5" className="tags">Mainnet</Tag> */}
+        {/* <Tag color="#2db7f5" className="tags">Testnet</Tag> */}
+        <Tag color="#2db7f5" className="tags">Mainnet</Tag>
 
       </div>
     );
